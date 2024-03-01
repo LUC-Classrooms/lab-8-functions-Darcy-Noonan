ec60ae4ab0bdba00615c4e413ef3ea7bd326b07e
Darcy-Noonan <156862020+Darcy-Noonan@users.noreply.github.com>
2024-03-01 16:30:38 +0000

    Completed Lab 8

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ function draw() {
   background(200);
 
   //this is a call to myShape()
-  myShape(width / 2, height / 2, 1);
+  myShape(width / 2, height /2, 1);
+
+
+  for (let i = 0; i < 10; i++)
+  myShape ( i * 50 + 30, 50, .5, i)
   // myShape() takes three arguments
-  // arg1: horizontal location
-  // arg2: vertical location
-  // arg3: scale factor (1 is full size)
-  /* add two more calls to myShape that draw your shape in different locations
-  */
+  // ((x, y, scale) )
+ myShape (width / 3, height /3, 1)
+ myShape (mouseX, mouseY, 1.5) // moves with mouse
+ 
   
 }
 
@@ -22,7 +25,11 @@ function myShape(x, y, s) {
   translate(x, y); // move the origin point
   scale(s);
   
-  ellipse(0, 0, w, h); // simple ellipse at the translated origin (0,0)
-  
+// MAKE SHAPES
+fill (50, 40, 250) // blue color
+ ellipse (100, 100, 50) // big ellipse
+ ellipse (90, 90, 10) // bubble effect
+
+
   pop(); // dispose of the layer
 }
